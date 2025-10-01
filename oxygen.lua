7229ba2449f3beafbff6adfdc6e2c9259186fd53
bw7dd <moneymant38@gmail.com>
2025-10-01 14:30:39 -0500

    Update oxygen.lua

--- oxygen.lua
+++ oxygen.lua
@@ -1276,7 +1276,7 @@ function Library.new(config)
 			SectionAutoUI.Parent = Section
 			SectionAutoUI.HorizontalAlignment = Enum.HorizontalAlignment.Center
 			SectionAutoUI.SortOrder = Enum.SortOrder.LayoutOrder
-			SectionAutoUI.Padding = UDim.new(0, 3.1982351)
+			SectionAutoUI.Padding = UDim.new(0, 3.198235951)
 
 			SectionAutoUI:GetPropertyChangedSignal('AbsoluteContentSize'):Connect(function()
 				Twen:Create(Section,TweenInfo.new(0.1),{
